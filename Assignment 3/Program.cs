daf1d7667588c3d502392a28b0f355a1b91813ea
omar <omaradel1114@gmail.com>
2026-07-31 18:18:28 +0300

    Add project files.

---
+++ Assignment 3/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Assignment_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] prices = { 25.5, 40.0, 33.75 };
+            Console.WriteLine(prices[1]);
+        }
+    }
+}
